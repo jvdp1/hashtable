7b41f684bae6fec5ad60c7860f8c2c453fe4d968
Vandenplas, Jeremie <jeremie.vandenplas@wur.nl>
2021-01-17 13:01:38 +0100

    formatting

--- src/modhash.f90
+++ src/modhash.f90
@@ -1,217 +1,216 @@
 module modhash
- use iso_fortran_env,only:int32,real32,real64
+ use iso_fortran_env, only: int32, real32, real64
  implicit none
  private
- public::hash,roundinguppower2
+ public :: hash, roundinguppower2
 
  integer(int32), parameter, public :: seed_hash = 305419896_int32
- 
+
  interface hash
   module procedure hashchar, hashint32, hashreal32, hashint32_scal, hashreal32_scal
  end interface
 
 contains
 
-pure function hashchar(k, seed) result(c)
- character(len=*), intent(in) :: k
- integer(int32), intent(in), optional :: seed
- integer(int32) :: c
- 
- integer(int32) :: lenk, length, i, j
- integer(int32), allocatable :: kint32(:)
+ pure function hashchar(k, seed) result(c)
+  character(len=*), intent(in) :: k
+  integer(int32), intent(in), optional :: seed
+  integer(int32) :: c
 
- lenk = len(k)
+  integer(int32) :: lenk, length, i, j
+  integer(int32), allocatable :: kint32(:)
 
- length = int(lenk/4_int32)
- if(mod(lenk,4).ne.0) length = length + 1
+  lenk = len(k)
 
- allocate(kint32(length))
+  length = int(lenk/4_int32)
+  if (mod(lenk, 4) .ne. 0) length = length + 1
 
- j=1
- do i=1,lenk,4
-  kint32(j) = transfer(k(i:min(i+3,lenk)),kint32(i))
-  j=j+1
- enddo
- 
- if(present(seed))then
-  c = hashint32(kint32, seed)
- else
-  c = hashint32(kint32)
- endif
-end function
+  allocate (kint32(length))
 
-pure function hashint32(k, seed) result(c)
- integer(kind=int32),intent(in)::k(:)
- integer(kind=int32), intent(in), optional :: seed
+  j = 1
+  do i = 1, lenk, 4
+   kint32(j) = transfer(k(i:min(i + 3, lenk)), kint32(i))
+   j = j + 1
+  end do
 
- integer(kind=int32) :: length
- integer(kind=int32) :: a,b,c
- integer(kind=int32) :: pos
- 
- integer(kind=int32) :: seed_
+  if (present(seed)) then
+   c = hashint32(kint32, seed)
+  else
+   c = hashint32(kint32)
+  end if
+ end function
 
- seed_ = seed_hash
- if(present(seed)) seed_ = seed
+ pure function hashint32(k, seed) result(c)
+  integer(kind=int32), intent(in) :: k(:)
+  integer(kind=int32), intent(in), optional :: seed
 
- length=size(k) 
+  integer(kind=int32) :: length
+  integer(kind=int32) :: a, b, c
+  integer(kind=int32) :: pos
 
- a=seed_ + ishft(length,2)
- b=a
- c=a
+  integer(kind=int32) :: seed_
 
- pos=1
- do while (length > 3)
-  a=a+k(pos)
-  b=b+k(pos+1)
-  c=c+k(pos+2)
-  call mix(a,b,c)
-  length=length-3
-  pos=pos+3
- end do 
+  seed_ = seed_hash
+  if (present(seed)) seed_ = seed
 
- select case(length)
-  case(1)
-   a=a+k(pos)
-  case(2)
-   a=a+k(pos)
-   b=b+k(pos+1)
-  case(3)
-   a=a+k(pos)
-   b=b+k(pos+1)
-   c=c+k(pos+2)
+  length = size(k)
+
+  a = seed_ + ishft(length, 2)
+  b = a
+  c = a
+
+  pos = 1
+  do while (length > 3)
+   a = a + k(pos)
+   b = b + k(pos + 1)
+   c = c + k(pos + 2)
+   call mix(a, b, c)
+   length = length - 3
+   pos = pos + 3
+  end do
+
+  select case (length)
+  case (1)
+   a = a + k(pos)
+  case (2)
+   a = a + k(pos)
+   b = b + k(pos + 1)
+  case (3)
+   a = a + k(pos)
+   b = b + k(pos + 1)
+   c = c + k(pos + 2)
   case default
- end select
+  end select
 
- call final(a,b,c)
+  call final(a, b, c)
 
-end function
+ end function
 
-pure function hashreal32(k, seed) result(c)
- real(kind=real32),intent(in)::k(:)
- integer(kind=int32), intent(in), optional :: seed
+ pure function hashreal32(k, seed) result(c)
+  real(kind=real32), intent(in) :: k(:)
+  integer(kind=int32), intent(in), optional :: seed
 
- integer(kind=int32) :: length
- integer(kind=int32) :: a,b,c
- integer(kind=int32) :: pos
- 
- integer(kind=int32) :: seed_
+  integer(kind=int32) :: length
+  integer(kind=int32) :: a, b, c
+  integer(kind=int32) :: pos
 
- seed_ = seed_hash
- if(present(seed)) seed_ = seed
+  integer(kind=int32) :: seed_
 
- length=size(k) 
+  seed_ = seed_hash
+  if (present(seed)) seed_ = seed
 
- a=seed_ + ishft(length,2)
- b=a
- c=a
+  length = size(k)
 
- pos=1
- do while (length > 3)
-  a=a+transfer(k(pos),a)
-  b=b+transfer(k(pos+1),b)
-  c=c+transfer(k(pos+2),c)
-  call mix(a,b,c)
-  length=length-3
-  pos=pos+3
- end do 
+  a = seed_ + ishft(length, 2)
+  b = a
+  c = a
 
- select case(length)
-  case(1)
-   a=a+transfer(k(pos),a)
-  case(2)
-   a=a+transfer(k(pos),a)
-   b=b+transfer(k(pos+1),b)
-  case(3)
-   a=a+transfer(k(pos),a)
-   b=b+transfer(k(pos+1),b)
-   c=c+transfer(k(pos+2),c)
+  pos = 1
+  do while (length > 3)
+   a = a + transfer(k(pos), a)
+   b = b + transfer(k(pos + 1), b)
+   c = c + transfer(k(pos + 2), c)
+   call mix(a, b, c)
+   length = length - 3
+   pos = pos + 3
+  end do
+
+  select case (length)
+  case (1)
+   a = a + transfer(k(pos), a)
+  case (2)
+   a = a + transfer(k(pos), a)
+   b = b + transfer(k(pos + 1), b)
+  case (3)
+   a = a + transfer(k(pos), a)
+   b = b + transfer(k(pos + 1), b)
+   c = c + transfer(k(pos + 2), c)
   case default
- end select
+  end select
 
- call final(a,b,c)
+  call final(a, b, c)
 
-end function
+ end function
 
-pure function hashint32_scal(k, seed) result(c)
- integer(int32), intent(in) :: k
- integer(int32), intent(in), optional :: seed
- integer(int32) :: c
- 
- integer(int32) :: lenk, length, i
- integer(int32), allocatable :: kint32(:)
+ pure function hashint32_scal(k, seed) result(c)
+  integer(int32), intent(in) :: k
+  integer(int32), intent(in), optional :: seed
+  integer(int32) :: c
 
- allocate(kint32(1))
- kint32 = k
- 
- if(present(seed))then
-  c = hashint32(kint32, seed)
- else
-  c = hashint32(kint32)
- endif
-end function
+  integer(int32) :: lenk, length, i
+  integer(int32), allocatable :: kint32(:)
 
-pure function hashreal32_scal(k, seed) result(c)
- real(real32), intent(in) :: k
- integer(int32), intent(in), optional :: seed
- integer(int32) :: c
- 
- integer(int32) :: lenk, length, i
- integer(int32), allocatable :: kint32(:)
+  allocate (kint32(1))
+  kint32 = k
 
- allocate(kint32(1))
- kint32(1) = transfer(k, kint32(1))
- 
- if(present(seed))then
-  c = hashint32(kint32, seed)
- else
-  c = hashint32(kint32)
- endif
-end function
+  if (present(seed)) then
+   c = hashint32(kint32, seed)
+  else
+   c = hashint32(kint32)
+  end if
+ end function
 
+ pure function hashreal32_scal(k, seed) result(c)
+  real(real32), intent(in) :: k
+  integer(int32), intent(in), optional :: seed
+  integer(int32) :: c
 
-elemental pure function roundinguppower2(x) result(next)
- integer(kind=int32),intent(in)::x
- integer(kind=int32)::next
+  integer(int32) :: lenk, length, i
+  integer(int32), allocatable :: kint32(:)
 
- !https://stackoverflow.com/questions/466204/rounding-up-to-next-power-of-2
- 
- next=2_int32**int((ceiling(log(real(x,real64))/log(real(2,real64)))),int32)
+  allocate (kint32(1))
+  kint32(1) = transfer(k, kint32(1))
 
-end function
+  if (present(seed)) then
+   c = hashint32(kint32, seed)
+  else
+   c = hashint32(kint32)
+  end if
+ end function
+
+ elemental pure function roundinguppower2(x) result(next)
+  integer(kind=int32), intent(in)::x
+  integer(kind=int32)::next
+
+  !https://stackoverflow.com/questions/466204/rounding-up-to-next-power-of-2
+
+  next = 2_int32**int((ceiling(log(real(x, real64))/log(real(2, real64)))), int32)
+
+ end function
 
 !PRIVATE
-    
-pure subroutine mix(a,b,c)
- integer(kind=int32),intent(inout)::a,b,c
 
- a=a-c;a=ieor(a,rot(c,4_int32));c=c+b 
- b=b-a;b=ieor(b,rot(a,6_int32));a=a+c 
- c=c-b;c=ieor(c,rot(b,8_int32));b=b+a 
- a=a-c;a=ieor(a,rot(c,16_int32));c=c+b 
- b=b-a;b=ieor(b,rot(a,19_int32));a=a+c 
- c=c-b;c=ieor(c,rot(b,4_int32));b=b+a 
+ pure subroutine mix(a, b, c)
+  integer(kind=int32), intent(inout) :: a, b, c
 
-end subroutine 
+  a = a - c; a = ieor(a, rot(c, 4_int32)); c = c + b
+  b = b - a; b = ieor(b, rot(a, 6_int32)); a = a + c
+  c = c - b; c = ieor(c, rot(b, 8_int32)); b = b + a
+  a = a - c; a = ieor(a, rot(c, 16_int32)); c = c + b
+  b = b - a; b = ieor(b, rot(a, 19_int32)); a = a + c
+  c = c - b; c = ieor(c, rot(b, 4_int32)); b = b + a
 
-pure subroutine final(a,b,c)
- integer(kind=int32),intent(inout)::a,b,c
+ end subroutine
 
- c=ieor(c,b); c=c-rot(b,14_int32)
- a=ieor(a,c); a=a-rot(c,11_int32)
- b=ieor(b,a); b=b-rot(a,25_int32)
- c=ieor(c,b); c=c-rot(b,16_int32)
- a=ieor(a,c); a=a-rot(c,4_int32)
- b=ieor(b,a); b=b-rot(a,14_int32)
- c=ieor(c,b); c=c-rot(b,24_int32)
+ pure subroutine final(a, b, c)
+  integer(kind=int32), intent(inout) :: a, b, c
 
-end subroutine
+  c = ieor(c, b); c = c - rot(b, 14_int32)
+  a = ieor(a, c); a = a - rot(c, 11_int32)
+  b = ieor(b, a); b = b - rot(a, 25_int32)
+  c = ieor(c, b); c = c - rot(b, 16_int32)
+  a = ieor(a, c); a = a - rot(c, 4_int32)
+  b = ieor(b, a); b = b - rot(a, 14_int32)
+  c = ieor(c, b); c = c - rot(b, 24_int32)
 
-pure function rot(i,j) result(rota)
- integer(kind=int32),intent(in):: i,j
- integer(kind=int32)::rota
+ end subroutine
 
- rota=ior(ishft(i,j),ishft(i,-(32_int32-j)))
+ pure function rot(i, j) result(rota)
+  integer(kind=int32), intent(in) :: i, j
+  integer(kind=int32) :: rota
 
-end function
+  rota = ior(ishft(i, j), ishft(i, -(32_int32 - j)))
+
+ end function
 
 end module
--- test/test_table.f90
+++ test/test_table.f90
@@ -1,5 +1,5 @@
 program test_table
- use modtable, only: tablechar_t, tableint32_t,tablereal32_t
+ use modtable, only: tablechar_t, tableint32_t, tablereal32_t
  implicit none
  type(tablechar_t) :: table
  type(tableint32_t) :: table32
@@ -43,6 +43,5 @@ program test_table
  call tabler32%add(2.)
  call tabler32%add(5.)
  call tabler32%writetable('tablereal32.dat')
-
 
 end program
