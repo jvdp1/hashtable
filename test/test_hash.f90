20d9cd2dac4642db57ce4cb3b87ca033f3799bc5
Jeremie Vandenplas <jeremie.vandenplas@gmail.com>
2021-01-19 19:42:45 +0100

    solve bug in modhash

--- test/test_hash.f90
+++ test/test_hash.f90
@@ -5,13 +5,13 @@ program test_list
  integer(int32) :: seed
 
  !print*,hash('aaa')
-! print*,hash(34)
+ !print*,hash(34)
  !print*,hash([1,2])
  !print*,hash(3.6_real32)
  !print*,hash([1.3, 4.5, 6.4])
 
- call check(hash('aaa') == -889379598, 'char')
- call check(hash('aaa    ') == -889379598, 'char trailing')
+ call check(hash('aaa') == 1769952254, 'char')
+ call check(hash('aaa    ') == 1769952254, 'char trailing')
  call check(hash(34) == -1461206935, 'int32')
  call check(hash([1, 2]) == -1956420251, 'int32_array')
  call check(hash(3.6_real32) == 596552866, 'real32')
@@ -25,13 +25,14 @@ program test_list
  !print*,hash(3.6_real32,1)
  !print*,hash([1.3, 4.5, 6.4],1)
 
+
  seed = 1
- call check(hash('aaa', seed) == -2045678163, 'char')
- call check(hash('aaa   ', seed) == -2045678163, 'char trailing')
- call check(hash(34, seed) == 912783350, 'int32')
- call check(hash([1, 2], seed) == -1736868386, 'int32_array')
- call check(hash(3.6_real32, seed) == -553270492, 'real32')
- call check(hash([1.3, 4.5, 6.4], seed) == 1124203056, 'real32 array')
+ call check(hash('aaa', seed) == -980364053, 'seed char')
+ call check(hash('aaa   ', seed) == -980364053, 'seed char trailing')
+ call check(hash(34, seed) == 912783350, 'seed int32')
+ call check(hash([1, 2], seed) == -1736868386, 'seed int32_array')
+ call check(hash(3.6_real32, seed) == -553270492, 'seed real32')
+ call check(hash([1.3, 4.5, 6.4], seed) == 1124203056, 'seed real32 array')
 
 contains
 
